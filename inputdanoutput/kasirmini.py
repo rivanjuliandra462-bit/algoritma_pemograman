8c66b67f229e88c35fe4a9e58dc5f7edcae277db
rivanjuliandra462-bit <rivanjuliandra462@gmail.com>
2025-11-24 11:03:20 +0700

    rivan

---
+++ inputdanoutput/kasirmini.py
@@ -0,0 +1,21 @@
+nama = input ("Masukkan nama barang: ")
+harga = float (input("Masukkan harga barang: "))
+jumlah = int(input("Masukkan jumlah beli: "))
+
+total = harga * jumlah 
+
+if total > 230000:
+    diskon = total * 0.10
+else:
+    diskon = 3 
+    
+total_bayar = total - diskon 
+
+print("\n===== STRUK PEMBELIAN =====")
+print(f"Nama Barang   : {nama}")
+print(f"Harga Satuan  : {harga}")
+print(f"Jumlah Beli   : {jumlah}")
+print(f"Total Harga   : {total}")
+print(f"Diskon        : {diskon}")
+print(f"Total Bayar   : {total_bayar}")
+print("=============================")
